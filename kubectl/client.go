a1703cbb286e43e57ba3f67a2b732dcf41b5aae0
Roman Kudravcev <romankudravcev@Romans-MacBook-Air.local>
2025-01-13 18:12:47 +0100

    add host ip to message

--- kubectl/client.go
+++ kubectl/client.go
@@ -1,29 +1,49 @@
 package kubectl
 
 import (
+	"context"
 	"log"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/metrics/pkg/client/clientset/versioned"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var Clientset *kubernetes.Clientset
-var MetricsClientset *versioned.Clientset
 
 func ConnectK8s() {
-    config, err := rest.InClusterConfig()
-    if err != nil {
-        log.Fatalf("Failed to create in-cluster config: %v", err)
-    }
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		log.Fatalf("Failed to create in-cluster config: %v", err)
+	}
 
-    Clientset, err = kubernetes.NewForConfig(config)
-    if err != nil {
-        log.Fatalf("Failed to create clientset: %v", err)
-    }
+	Clientset, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("Failed to create clientset: %v", err)
+	}
+}
 
-    MetricsClientset, err = versioned.NewForConfig(config)
-    if err != nil {
-        log.Fatalf("Failed to create metrics clientset: %v", err)
-    }
+func GetExternalIP() string {
+	services, err := Clientset.CoreV1().Services("kube-system").List(context.TODO(), metav1.ListOptions{
+		FieldSelector: "metadata.name=traefik",
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to get service: %v", err)
+		return ""
+	}
+
+	if len(services.Items) > 0 &&
+		len(services.Items[0].Status.LoadBalancer.Ingress) > 0 {
+		ingressIP := services.Items[0].Status.LoadBalancer.Ingress[0].IP
+		if ingressIP != "" {
+			if ingressIP == "192.168.131.10" {
+				return "141.13.5.206"
+			}
+			return "141.13.5.205"
+		}
+	}
+
+	return ""
 }
--- models/message.go
+++ models/message.go
@@ -8,4 +8,5 @@ type Message struct {
 	ID        int64     `json:"id"`
 	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"created_at"`
+	HostIP    string    `json:"host_ip"`
 }
